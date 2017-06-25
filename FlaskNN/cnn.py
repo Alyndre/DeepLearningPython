b6d993ce91d9fbd20970cba977e30a5109d98a9e
Shauku <n3rv.apps@gmail.com>
2017-06-26 00:46:29 +0200

    expanding train method

--- FlaskNN/cnn.py
+++ FlaskNN/cnn.py
@@ -9,7 +9,7 @@ class ConvLayer(object):
         self.weights = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
         self.biases = tf.Variable(tf.constant(0.1, shape=[output_c]))
 
-    def forward(self, X, pool=false):
+    def forward(self, X, pool=False):
         convolution = tf.nn.conv2d(X, self.weights, strides=[1, 1, 1, 1], padding='SAME')
         convolution = tf.nn.bias_add(convolution, self.biases)
         if pool:
@@ -46,7 +46,7 @@ class CNN(object):
             layer = HiddenLayer(input_c, output_c)
             self.hidden_layers.append(layer)
 
-    def predict(self, X):
+    def forward(self, X):
         Z = X
         for convolution in self.convpool_layers:
             Z = convolution.forward(Z, pool=[2, 2]) # Can add pooling here
@@ -58,15 +58,52 @@ class CNN(object):
             Z = hidden.forward(Z)
 
         Z = tf.matmul(Z, self.W) + self.B
+        return Z
+
+    def predict(self, X):
+        Z = self.forward(X)
         prediction = tf.argmax(Z, 1)
         return prediction
 
-    def train(self):
+    def train(self, epochs, batch_sz, learning_rate = 10e-4, decay = 0.99999, momentum = 0.99):
+        X, Y = getImageData()
+        X, Y = shuffle(X, Y)
+        X = X.astype(np.float32)
+        Y = y2indicator(Y).astype(np.float32)
+        # reshape X for tf: N x w x h x c
+        X = X.transpose((0, 2, 3, 1))
+        N, width, height, c = X.shape
 
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=TARGETS, logits=OUTPUT))
-        train_step = tf.train.RMSPropOptimizer(10e-4, decay=0.99999, momentum=0.99).minimize(cross_entropy) # tf.train.AdamOptimizer(1e-2).minimize(cross_entropy)
+
+        Xvalid, Yvalid = X[-1000:], Y[-1000:]
+        X, Y = X[:-1000], Y[:-1000]
+        Yvalid_flat = np.argmax(Yvalid, axis=1) # for calculating error rate
+
+        INPUTS = tf.placeholder(tf.float32, shape=(None, width, height, c))
+        TARGETS = tf.placeholder(tf.float32, shape=(None, 7))
+
+        OUTPUTS = self.forward(INPUTS)
+        prediction = self.predic(INPUTS)
+
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=TARGETS, logits=OUTPUTS))
+        train_step = tf.train.RMSPropOptimizer(learning_rate, decay, momentum).minimize(cross_entropy) # tf.train.AdamOptimizer(1e-2).minimize(cross_entropy)
 
         # Compare network output vs target
         correct_prediction = tf.equal(prediction, tf.argmax(TARGETS,1))
         # Cast bools to float32 and take the mean
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        n_batches = N // batch_sz
+        with tf.Session() as session:
+            session.run(tf.global_variables_initializer())
+            for e in range(epochs):
+                for i in range(n_batches):
+                    Xbatch = X[i*batch_sz:(i*batch_sz+batch_sz)]
+                    Ybatch = Y[i*batch_sz:(i*batch_sz+batch_sz)]
+                    train_step.run(feed_dict={INPUTS: Xbatch, TARGETS: Ybatch})
+                    if (i % 20 == 0):
+                        #train_accuracy = accuracy.eval(feed_dict={INPUTS: Xvalid, TARGETS: Yvalid, keep_prob: 1.0})
+                        p = session.run(prediction, feed_dict={INPUTS: Xvalid, TARGETS: Yvalid})
+                        error = np.mean(Yvalid_flat != p)
+                        acc = accuracy.eval(feed_dict={INPUTS: Xvalid, TARGETS: Yvalid})
+                        print("step: ", e, " error rate: ", error, " accuracy: ", acc)
